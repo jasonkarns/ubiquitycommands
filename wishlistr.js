ba2a4932937b71a2e544c81fcf97fa298a885573
Jason Karns <jason@karns.name>
2010-02-20 18:48:49 -0500

    Updated command to use the new Parser 2 API

--- wishlistr.js
+++ wishlistr.js
@@ -1,28 +1,32 @@
 CmdUtils.CreateCommand({ 
-  name: "wishlistr",
+  name: ["wishlist", "wishlistr", "add to wishlistr"],
   icon: "http://www.wishlistr.com/favicon.ico",
   homepage: "http://jason.karns.name/",
   author: { name: "Jason Karns", email: "jason@karns.name"},
   license: "GPL",
   description: "Add an item to your Wishlistr wishlist.",
   help: "Highlight some text to be used as a title and Wishlistr will do the rest!",
-  takes: {"item title": noun_arb_text},
-  preview: function( pblock, input ) {
+  'arguments': [
+    {role: 'alias', nountype: noun_arb_text, label: 'Title'}
+  ],
+  preview: function(pblock, args) {
+    var data = {
+      "title": (args.alias && args.alias.text)? args.alias.text : context.focusedWindow.document.title,
+      "link": context.focusedWindow.document.location
+    };
     var template = 
-		"<p>Add item to your Wishlist.</p>" +
-		"<dl><dt>Title:</dt><dd>${title}</dd>" +
-		"<dt>Link:</dt><dd>${link}</dd></dl>";
-    pblock.innerHTML = CmdUtils.renderTemplate(template, {
-		"title": input.text || context.focusedWindow.document.title,
-		"link": context.focusedWindow.document.location
-	});
+    "<p>Add item to your Wishlist.</p>" +
+    "<dl><dt>Title:</dt><dd>${title}</dd>" +
+    "<dt>Link:</dt><dd>${link}</dd></dl>";
+    pblock.innerHTML = CmdUtils.renderTemplate(template, data);
   },
-  execute: function(input) {
+  execute: function(args) {
     var href = context.focusedWindow.document.location;
+    var title = (args.alias && args.alias.text)? args.alias.text : context.focusedWindow.document.title;
     var url = 'http://www.wishlistr.com/tools/bookmarklet.php?v=1&u='
-		+ encodeURIComponent(href)
-		+ '&t='
-		+ encodeURIComponent(input.text || context.focusedWindow.document.title);
-    Utils.openUrlInBrowser( url );
+      + encodeURIComponent(href)
+   	  + '&t='
+      + encodeURIComponent(title);
+    Utils.openUrlInBrowser(url);
   }
 });
