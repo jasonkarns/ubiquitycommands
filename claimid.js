98fa1347c014fff620e3f2388a765d466f074525
Jason <Jason@10.99.1.241>
2010-02-20 16:48:10 -0500

    Updated command to work with Parser 2

--- claimid.js
+++ claimid.js
@@ -1,28 +1,32 @@
 CmdUtils.CreateCommand({ 
-  name: "claimid",
+  name: ["claim", "claimid", "send to claimid", "add to claimid"],
   icon: "http://claimid.com/images/favicon.ico",
   homepage: "http://jason.karns.name/",
   author: { name: "Jason Karns", email: "jason@karns.name"},
   license: "GPL",
   description: "Post to ClaimID",
   help: "Highlight some text to be used as a title and ClaimID will do the rest!",
-  takes: {"item title": noun_arb_text},
-  preview: function( pblock, input ) {
+  'arguments': [
+    {role: 'alias', nountype: noun_arb_text, label: 'Title'}
+  ],
+  preview: function(pblock, args) {
+    var data = {
+      "title": (args.alias && args.alias.text)? args.alias.text : context.focusedWindow.document.title,
+      "link": context.focusedWindow.document.location
+    };
     var template = 
-		"<p>Post item to ClaimID.</p>" +
-		"<dl><dt>Title:</dt><dd>${title}</dd>" +
-		"<dt>Link:</dt><dd>${link}</dd></dl>";
-    pblock.innerHTML = CmdUtils.renderTemplate(template, {
-		"title": input.text || context.focusedWindow.document.title,
-		"link": context.focusedWindow.document.location
-    });
+    "<p>Post item to ClaimID.</p>" +
+    "<dl><dt>Title:</dt><dd>${title}</dd>" +
+    "<dt>Link:</dt><dd>${link}</dd></dl>";
+    pblock.innerHTML = CmdUtils.renderTemplate(template, data);
   },
-  execute: function(input) {
+  execute: function(args) {
     var href = context.focusedWindow.document.location;
+  var title = (args.alias && args.alias.text)? args.alias.text : context.focusedWindow.document.title;
     var url = 'http://claimid.com/post/?url='
         + encodeURIComponent(href)
         + '&title='
-        + encodeURIComponent(input.text || context.focusedWindow.document.title);
+        + encodeURIComponent(title);
     Utils.openUrlInBrowser( url );
   }
 });
